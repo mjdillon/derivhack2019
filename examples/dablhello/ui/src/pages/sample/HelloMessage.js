a220a6ab36ba01c56de64a6e4421032d421d8da8
Dimitri Liakakos <dimitri.liakakos@digitalasset.com>
2019-10-07 19:55:38 -0400

    Remove Accept and Reject Message choice

--- examples/dablhello/ui/src/pages/sample/HelloMessage.js
+++ examples/dablhello/ui/src/pages/sample/HelloMessage.js
@@ -8,43 +8,19 @@ export default function HelloMessage() {
   const user = useUserState();
   const ledger = useLedgerState();
   const ledgerDispatch = useLedgerDispatch();
-  const greeters = getContracts(ledger, "Sample", "HelloMessage");
-
-  const acceptChoice = async (c) => {
-    const command = {
-      templateId: { moduleName: "Sample", entityName: "HelloMessage" },
-      contractId: c.contractId,
-      choice: "AcceptMessage",
-      argument: {},
-      meta: { ledgerEffectiveTime: 0 }
-    };
-    await sendCommand(ledgerDispatch, user.token, "exercise", command, () => {}, () => {});
-    await fetchContracts(ledgerDispatch, user.token, () => {}, () => {});
-  }
-
-  const rejectChoice = async (c) => {
-    const command = {
-      templateId: { moduleName: "Sample", entityName: "HelloMessage" },
-      contractId: c.contractId,
-      choice: "RejectMessage",
-      argument: {},
-      meta: { ledgerEffectiveTime: 0 }
-    };
-    await sendCommand(ledgerDispatch, user.token, "exercise", command, () => {}, () => {});
-    await fetchContracts(ledgerDispatch, user.token, () => {}, () => {});
-  }
+  const helloMessages = getContracts(ledger, "Sample", "HelloMessage");
 
   return (
     <>
       <Contracts
-        contracts={greeters}
+        contracts={helloMessages}
         columns={[
           ["ContractId", "contractId"],
           ["Sender", "argument.sender"],
           ["Recipient", "argument.recipient"],
           ["Message", "argument.message"],
         ]}
-        actions={[["Accept Message", acceptChoice], ["Reject Message", rejectChoice]]}
+        actions={[]}
       />
     </>
   );
